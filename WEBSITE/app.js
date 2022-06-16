21bc38750924c3acb752224661be9ce8c828d1fb
Saurav Gurhale <slgurhal@uwaterloo.ca>
2022-06-16 15:37:58 -0400

    fixed EDI page

--- WEBSITE/app.js
+++ WEBSITE/app.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const path = require('path');
 const { MongoClient } = require('mongodb');
-const PORT = 5000;
+const PORT = 3000;
 
 // create express app
 const app = express();
